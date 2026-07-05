24545a008b3d38aa17ea4d565dd9bcd7fe9acb3c
HishantK <kudalkarhishant@gmail.com>
2026-07-05 10:10:28 +0530

    testing combat for game

---
+++ combat/scripts/player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+
+var SPEED = 500.0
+const JUMP_VELOCITY = -500.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var attacking = false
+var jumpeda = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack") and !attacking and is_on_floor():
+		animated_sprite_2d.play("attack")
+		attacking = true
+		SPEED = 300.0
+	if Input.is_action_just_pressed("attack") and !attacking and !is_on_floor():
+		animated_sprite_2d.play("attack 2")
+		attacking = true
+		SPEED = 300.0
+	if Input.is_action_just_released("attack"):
+		attacking = false
+		SPEED = 500.0
+		
+			
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jumpeda = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	if direction == 0 && !attacking:
+		animated_sprite_2d.play("default")
+	elif !attacking:
+		animated_sprite_2d.play("run")
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
