6577a3854d088fa66ca7cc5acf6afc511fbb82dc
jiyac-code <jiyac1019@gmail.com>
2026-09-19 15:34:57 +0530

    Updated Level 3

--- level/scripts/Level3/refugee.gd
+++ level/scripts/Level3/refugee.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var player_near =false
+var dialogue_started := false
 @onready var player: CharacterBody2D = $"../Player"
 
 func _ready():
@@ -8,7 +9,8 @@ func _ready():
 	body_exited.connect(on_body_exited)
 	
 func _process(_delta):
-	pass
+	if player_near and Input.is_action_just_pressed("interact"):
+		start_dialogue()
 	
 func on_body_entered(body):
 	if body.name == "Player":
@@ -17,3 +19,20 @@ func on_body_entered(body):
 func on_body_exited(body):
 	if body.name == "Player":
 		player_near = false
+
+func start_dialogue():
+	if dialogue_started:
+		return
+
+	dialogue_started = true
+	player.can_move = false
+	player.animated_sprite_2d.stop()
+
+	Dialogic.start("refugee-child")
+
+	await Dialogic.timeline_ended
+
+	player.can_move = true
+	dialogue_started = false
+	if Dialogic.VAR.save_child:
+		QuestManager.start_quest("rescue_child")
