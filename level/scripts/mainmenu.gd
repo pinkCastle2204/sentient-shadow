0b222906b70cc7c4d6225f24bfe8055369bec843
HishantK <kudalkarhishant@gmail.com>
2026-09-26 14:44:19 +0530

    Intro and control panel

--- level/scripts/mainmenu.gd
+++ level/scripts/mainmenu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +15,11 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1_intro.tscn")
 	
+
+
+func _on_controls_pressed() -> void:
+	canvas_layer.visible = true
+
+
+func _on_button_pressed() -> void:
+	canvas_layer.visible = false
